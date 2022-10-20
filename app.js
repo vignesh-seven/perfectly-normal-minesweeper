85b4088e472c8f229146978090c3b34659ffdc66
vignesh-seven <vignesh77024@gmail.com>
2022-10-20 12:01:13 +0530

    perfectly normal shape drawing working (at a fixed location)

--- app.js
+++ app.js
@@ -1,10 +1,71 @@
 // constants
 const body =  document.body
 const tbl = document.querySelector('.board')
-const size = 16
+const size = 20
 
 const theRedColor = getComputedStyle(document.documentElement).getPropertyValue('--the-red');
 const theBGColor = getComputedStyle(document.documentElement).getPropertyValue('--the-background-color');
+
+// sus shape locations 11 x 11
+const susShapeCords = [
+  [0,2],
+  [0,3],
+  [0,4],
+  [0,5],
+  [0,6],
+
+  [1,1],
+  [1,7],
+
+  [2,0],
+  [2,8],
+
+  [3,0],
+  [3,3],
+  [3,4],
+  [3,5],
+  [3,6],
+  [3,7],
+  [3,8],
+
+  [4,0],
+  [4,2],
+  [4,9],
+
+  [5,0],
+  [5,2],
+  [5,9],
+
+  [6,0],
+  [6,3],
+  [6,4],
+  [6,5],
+  [6,6],
+  [6,7],
+  [6,8],
+
+  [7,0],
+  [7,8],
+
+  [8,0],
+  [8,8],
+  
+  [9,0],
+  [9,4],
+  [9,8],
+
+  [10,0],
+  [10,3],
+  [10,5],
+  [10,8],
+
+  [11,1],
+  [11,2],
+  [11,6],
+  [11,7],
+]
+
+
 
 // calculate size of the board
   const viewLength = window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth
@@ -13,20 +74,24 @@ const theBGColor = getComputedStyle(document.documentElement).getPropertyValue('
 // 💣
 
 
-
 // generating the board 
 let bombValues = []
 
 for (let i=0; i<size; i++) {
-  bombValues[i] = [];
+  bombValues[i] = [false];
+}
+
+// place the sus shape
+
+for (let i=0; i<susShapeCords.length; i++) {
+  bombValues[susShapeCords[i][0]][susShapeCords[i][1]] = true
 }
 
 for (let i = 0; i < size; i++) {
   for (let j = 0; j < size; j++) {
-    if (Math.random() < 0.2) {
+    if (bombValues[i][j]) { continue; }
+    if (Math.random() < 0.1) {
       bombValues[i][j] = true
-    } else {
-      bombValues[i][j] = false
     }
   }
 }
@@ -40,18 +105,23 @@ for (let i=0; i<size; i++) {
 
 
 // generating the board and placing bombs
-function fillTheBombs() {  // cell creation also happens here
+function createTable() {  // cell creation also happens here
   for (let i = 0; i < size; i++) {
     const tr = tbl.insertRow();
     for (let j = 0; j < size; j++) {
       const td = tr.insertCell();
+      // place the bomb
       if (bombValues[i][j]) {
-        td.appendChild(document.createTextNode(`💣`));
+
+         td.appendChild(document.createTextNode(`💣`));
+        td.style.backgroundColor = theRedColor
+        
       }
-      td.classList.add("cell")
+      // td.classList.add("cell")
       td.style.width = `${cellSize}px`
       td.style.height = `${cellSize}px`
       td.align = "center"
+      // console.log(td.style.backgroundColor)
 
     }
   }
@@ -88,10 +158,10 @@ function fillTheValues() {
   }
 }
 
-fillTheBombs()
+createTable()
 fillTheValues()
 
-// console.table(bombValues)  // important
+console.table(bombValues)  // important
 // console.table(bombNumbers) // important
 
 // on right-click event (flag)
@@ -112,6 +182,7 @@ tbl.addEventListener('mousedown', (e) => {
   if(bombValues[cellY][cellX]) {
 
     // game over
+    e.target.innerText = `💣`
     console.log("bomb exploded at X: " + cellX + " Y: " + cellY + " game over")
     
   } else {
@@ -127,9 +198,4 @@ tbl.addEventListener('mousedown', (e) => {
 
 })
 
-// prevent drag & drop errors
-// tbl.addEventListener('dragstart drop', () => {
-//   e.preventDefault();
-//   return false;
-// })
 
